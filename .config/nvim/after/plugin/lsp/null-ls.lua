e37606d9304b8793f2d868817d454b80fed10133
Andrei Nasui <andreinasui1@gmail.com>
2023-11-08 11:00:55 +0200

    device specific update

--- .config/nvim/after/plugin/lsp/null-ls.lua
+++ .config/nvim/after/plugin/lsp/null-ls.lua
@@ -14,6 +14,7 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
+	debug = true,
 	-- setup formatters & linters
 	sources = {
 		--  formatters
