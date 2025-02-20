9d055f4a5cd936f47c4b834062ccaadaa6228755
Andrei Nasui <andreinasui1@gmail.com>
2025-02-20 13:00:24 +0200

    for hide waybar append X instead of 0

--- hyprland/.config/hypr/scripts/hide_waybar.sh
+++ hyprland/.config/hypr/scripts/hide_waybar.sh
@@ -9,12 +9,12 @@ fi
 
 toggle_bar() {
 	active_monitor=$(hyprctl activeworkspace | awk 'NR==1{sub(/:/, ""); print $NF }')
-	if grep -q "$active_monitor""0" "$waybar_config_file"; then
-		# Remove the 0 from active monitor name - this will make it valid and will show the bar on that display
-		sed -i -E "/^ *\"output\": *\[.*\"""$active_monitor""0\".*\],$/s/""$active_monitor""0/$active_monitor/g" "$waybar_config_file"
+	if grep -q "$active_monitor""X" "$waybar_config_file"; then
+		# Remove the X from active monitor name - this will make it valid and will show the bar on that display
+		sed -i -E "/^ *\"output\": *\[.*\"""$active_monitor""X\".*\],$/s/""$active_monitor""X/$active_monitor/g" "$waybar_config_file"
 	elif grep -q "$active_monitor" "$waybar_config_file"; then
-		# Append a 0 to active monitor nam - this will make it invalid and will hide the bar on that displaye
-		sed -i -E "/^ *\"output\": *\[.*\"$active_monitor\".*\],$/s/$active_monitor/\00/g" "$waybar_config_file"
+		# Append an X to active monitor nam - this will make it invalid and will hide the bar on that displaye
+		sed -i -E "/^ *\"output\": *\[.*\"$active_monitor\".*\],$/s/$active_monitor/\0X/g" "$waybar_config_file"
 	else
 		return 0
 	fi
