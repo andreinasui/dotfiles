a33b2d0cf8a7ca64d9864ebda5ca783dae61eb11
Andrei Nasui <andreinasui1@gmail.com>
2025-02-24 23:52:21 +0200

    hide_waybar script update

--- hyprland/.config/hypr/scripts/hide_waybar.sh
+++ hyprland/.config/hypr/scripts/hide_waybar.sh
@@ -10,8 +10,9 @@ fi
 toggle_bar() {
 	active_monitor=$(hyprctl activeworkspace | awk 'NR==1{sub(/:/, ""); print $NF }')
 	if grep -q "$active_monitor""X" "$waybar_config_file"; then
-		# Remove the X from active monitor name - this will make it valid and will show the bar on that display
-		sed -i -E "/^ *\"output\": *\[.*\"""$active_monitor""X\".*\],$/s/""$active_monitor""X/$active_monitor/g" "$waybar_config_file"
+		# Copy original file over tmp working file - doing it this way so
+		# if I make changes to original file I can easily update the tmp working file
+		cp "$waybar_config_file_original" "$waybar_config_file"
 	elif grep -q "$active_monitor" "$waybar_config_file"; then
 		# Append an X to active monitor nam - this will make it invalid and will hide the bar on that displaye
 		sed -i -E "/^ *\"output\": *\[.*\"$active_monitor\".*\],$/s/$active_monitor/\0X/g" "$waybar_config_file"
